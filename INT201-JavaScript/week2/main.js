8d6566d71f5fd96adab8425ce2e38915c8ae0dfb
realFK98 <wishira2013.coth@gmail.com>
2023-08-17 17:06:34 +0700

    f

---
+++ INT201-JavaScript/week2/main.js
@@ -0,0 +1,42 @@
+console.log('my name is umaporn Supasitthimethee');
+setTimeout(() => console.log("Task 1 : complex task is done."), 5000) //setTimeout is higher function , ()=> is callback function
+console.log('Good Morning');
+setTimeout(() => console.log("Task 2 : complex task is done."), 2000)
+console.log('Good Bye');
+
+let name1;
+
+
+
+
+
+
+
+
+
+
+
+// function greeting(someone) {
+//     return `hello ${someone}`;
+// }
+// console.log(greeting("he"));
+
+// let name = 100;
+// console.log(name);
+// name = 'my name is umaporn Supasitthimethee';
+// console.log(name);
+
+
+// const std = {
+//     id: 65,
+//     name: "wichira"
+// }
+// console.log(std);
+// const nums = [5, 10, 15, 20]
+
+// std.sayHello = "kuy"
+
+// console.log(std.__proto__);
+// console.log(std.sayHello);
+
+
